fa7b438c8f1462123fd178ae36c59bb95365ae1f
arifanf <79965177+arifanf@users.noreply.github.com>
2022-05-25 21:16:21 +0700

    Initial Commit

---
+++ Components/List/Header.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import CoverImage from './CoverImage'
+import Meta from './Meta'
+
+const Header = () => {
+  return (
+    <>
+        <CoverImage />
+        <Meta />
+    </>
+  )
+}
+
+export default Header
